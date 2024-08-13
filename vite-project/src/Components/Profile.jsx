2da4fd572be2e06e807e731e900e6bf24d3af55c
DevRamona <r.ingabire@alustudent.com>
2024-08-13 11:56:52 +0200

    profile & login

---
+++ vite-project/src/Components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Profile() {
+  return ( 
+    <><div>Profile page</div>
+    <p>Name : </p>
+    <p>Age : </p>
+    <p>Email : </p></>
+    
+  )
+}
+
+export default Profile
